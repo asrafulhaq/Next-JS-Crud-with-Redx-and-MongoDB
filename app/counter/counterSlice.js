088547b278deb110539d1066d4c1dfa9c006eb27
asrafulhaq <devvorb@gmail.com>
2023-06-07 22:46:30 +0600

    Counter project done

--- app/counter/counterSlice.js
+++ app/counter/counterSlice.js
@@ -6,12 +6,19 @@ const counterSlice = createSlice({
   initialState: {
     count: 0,
   },
-  reducers: {},
+  reducers: {
+    increment: (state, action) => {
+      state.count++;
+    },
+    decrement: (state, action) => {
+      state.count--;
+    },
+  },
 });
 
 // selectors
-
+export const selectCount = (state) => state.counter;
 // actions
-export const {} = counterSlice.actions;
+export const { increment, decrement } = counterSlice.actions;
 // reducer
 export default counterSlice.reducer;
--- app/counter/page.jsx
+++ app/counter/page.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+"use client";
+
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, selectCount } from "./counterSlice";
 
 const CounterPage = () => {
+  const { count } = useSelector(selectCount);
+  const dispatch = useDispatch();
   return (
     <div>
-      <h1>0</h1>
+      <h1>{count}</h1>
       <hr />
-      <button>--</button>
-      <button>++</button>
+      <button onClick={() => dispatch(decrement())}>--</button>
+      <button onClick={() => dispatch(increment())}>++</button>
     </div>
   );
 };
